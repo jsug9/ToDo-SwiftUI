613500babfab9a969e1004fd771f5c6165b54cfe
jsug9 <75434179+jsug9@users.noreply.github.com>
2021-06-15 12:32:13 -0500

    Initial Commit

---
+++ ToDo SUI Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDo SUI Test
+//
+//  Created by Augusto Galindo Alí on 15/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ToDo SUI Test/ToDo_SUI_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_SUI_TestApp.swift
+//  ToDo SUI Test
+//
+//  Created by Augusto Galindo Alí on 15/06/21.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_SUI_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
